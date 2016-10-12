2f1d1add91e29f4f74ab0727de93e7cc392b9fbc
Michaël Zasso <targos@protonmail.com>
2016-10-12 11:13:00 +0200

    use yarn upgrade to have fast dependency install

--- src/server/util/git.js
+++ src/server/util/git.js
@@ -52,7 +52,7 @@ Git.prototype.pull = makeTask('pull', pullRepo);
 function*doBuild() {
     yield this.pull();
     debug('building project');
-    yield child_process.execFile('npm', ['update'], this.execOptions);
+    yield child_process.execFile('yarn', ['upgrade'], this.execOptions);
     yield child_process.execFile('npm', ['run', 'build'], this.execOptions);
     debug('build finished, getting list of files');
     try {
