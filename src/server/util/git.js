f9d6ca5a4cbeb3337d0b760b0b08383582a2ac1d
Michaël Zasso <targos@protonmail.com>
2019-11-12 09:17:46 +0100

    fix: use current branch to reset

--- src/server/util/git.js
+++ src/server/util/git.js
@@ -45,9 +45,16 @@ function* pullRepo(reset) {
   yield this.init();
   debug('pulling from GitHub');
   if (reset) {
+    const currentBranch = (yield child_process.execFile(
+      'git',
+      ['rev-parse', '--abbrev-ref', 'HEAD'],
+      this.execOptions,
+    ))
+      .toString()
+      .trim();
     yield child_process.execFile(
       'git',
-      ['reset', '--hard', 'origin/master'],
+      ['reset', '--hard', `origin/${currentBranch}`],
       this.execOptions,
     );
     yield child_process.execFile('git', ['clean', '-fd'], this.execOptions);
